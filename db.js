9943894f9f2fd669ad10dd2cde75c5ff551584b4
Deepak Bisht <db362966@gmail.com>
2026-03-07 15:43:29 +0530

    updated version

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 //define the mongodb connection url
 //const mongoURL = process.env.MONGO_URL_LOCAL
-const mongoURL = process.env.MONGO_URL;
+const mongoURL = process.env.MONGODB_URL;
 //setup mongodb conecion
 mongoose.connect(mongoURL);
     
